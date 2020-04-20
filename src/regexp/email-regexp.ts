ea0dd3d3b499eb18a020f09cefeafa429529de08
wangcylive <wangcylive@outlook.com>
2020-04-20 17:29:19 +0800

    feat:email regexp

---
+++ src/regexp/email-regexp.ts
@@ -0,0 +1,2 @@
+const emailRegExp = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/ig
+export default emailRegExp
